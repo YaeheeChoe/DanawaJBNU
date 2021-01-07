418bab9516d1a0e057b87d58ac628fb6c100a99c
YaeheeChoe <72256237+YaeheeChoe@users.noreply.github.com>
2021-01-07 19:42:23 +0900

    JBNU 하이퍼링크 기능 셋팅

--- whale/popup/popup.js
+++ whale/popup/popup.js
@@ -1,4 +1,4 @@
-const JBNUHomeURL = "http://https://www.jbnu.ac.kr";
+const JBNUHomeURL = "https://www.jbnu.ac.kr";
 const PotalURL = "http://portal.jbnu.ac.kr";
 const GitRepoURL = "https://github.com/seohyun0120/quick-sook";
 const OasisURL = "https://all.jbnu.ac.kr/jbnu/oasis";
@@ -13,9 +13,9 @@ function openURLInNewTab(url) {
 
 document.getElementById('clickJBNU').addEventListener('click', () => { openURLInNewTab(JBNUHomeURL) }, false);
 document.getElementById('clickPotal').addEventListener('click', () => { openURLInNewTab(PotalURL) }, false);
-document.getElementById('clickGitRepo').addEventListener('click', () => { openURLInNewTab(GitRepoURL) }, false);
 document.getElementById('clickOasis').addEventListener('click', () => { openURLInNewTab(OasisURL) }, false);
 document.getElementById('clickCalendar').addEventListener('click', () => { openURLInNewTab(CalURL) }, false);
 document.getElementById('clickMap').addEventListener('click', () => { openURLInNewTab(MapURL) }, false);
 document.getElementById('clickLibrary').addEventListener('click', () => { openURLInNewTab(LibraryURL) }, false);
 document.getElementById('clickLunchroom').addEventListener('click', () => { openURLInNewTab(lunchroomURL) }, false);
+document.getElementById('clickEasterEgg').addEventListener('click', () => { openURLInNewTab(GitRepoURL) }, false);
